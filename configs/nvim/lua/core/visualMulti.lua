a017b8961103cb53e085fdbbe72a13e6cb124aae
glowfi <86939591+glowfi@users.noreply.github.com>
2026-02-08 19:21:16 +0530

    add initial dotfiles

---
+++ configs/nvim/lua/core/visualMulti.lua
@@ -0,0 +1,6 @@
+-- Keymappings
+vim.cmd("let g:VM_maps = {}")
+vim.cmd("let g:VM_default_mappings = 0")
+vim.cmd("let g:VM_maps['Add Cursor Down']          = '<C-c-Down>'")
+vim.cmd("let g:VM_maps['Add Cursor Up']            = '<C-c-Up>'")
+vim.cmd("let g:VM_maps['Find Under']               = '<C-n>'")
