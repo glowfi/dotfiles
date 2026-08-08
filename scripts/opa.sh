a21681fda8a48bc25076a7ac1ab2b5a704230566
glowfi <86939591+glowfi@users.noreply.github.com>
2026-08-08 17:31:57 +0530

    fix kitty config path in opa.sh

--- scripts/opa.sh
+++ scripts/opa.sh
@@ -3,7 +3,7 @@
 line_check=$(sed -n '47p' $HOME/.config/kitty/kitty.conf)
 
 if [[ "$line_check" == "## Tabs" ]]; then
-	cp -r $HOME/setup/configs/kitty/kitty.conf $HOME/.config/kitty/
+	cp -r $HOME/.dotfiles/configs/kitty/ $HOME/.config
 else
 	sed -i '46,50d' $HOME/.config/kitty/kitty.conf
 fi
